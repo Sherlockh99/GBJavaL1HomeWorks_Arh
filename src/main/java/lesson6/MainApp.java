32c57af4e4595ffd8107298393e3890bcab8cdf7
Sherlockh99 <sherlockh989@gmail.com>
2021-10-12 22:33:04 +0300

    Homework 6

---
+++ src/main/java/lesson6/MainApp.java
@@ -0,0 +1,7 @@
+package lesson6;
+
+public class MainApp {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/lesson6/animals/Animal.java
@@ -0,0 +1,11 @@
+package lesson6.animals;
+
+public class Animal {
+    protected String name;
+    protected int age;
+    protected String color;
+
+    public Animal(String name, int age, String color){
+
+    }
+}
