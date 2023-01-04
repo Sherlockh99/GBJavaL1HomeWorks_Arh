2472582c2971b48ac0e8c80a13f8ecb4c8a6dd85
Sherlockh99 <sherlockh989@gmail.com>
2023-01-04 23:49:08 +0200

    Translate to English and added tasks for lesson7.java

--- src/main/java/lesson7/Cat.java
+++ src/main/java/lesson7/Cat.java
@@ -3,18 +3,8 @@ package lesson7;
 public class Cat {
     private String name;
     private int appetite;
-    //private boolean isHungry;
     private boolean satiety;
 
-    /*
-    public boolean isHungry() {
-        return isHungry;
-    }
-
-    public void setHungry(boolean hungry) {
-        isHungry = hungry;
-    }
-*/
 
     public boolean isSatiety() {
         return satiety;
@@ -41,24 +31,19 @@ public class Cat {
 
     public void eat(Plate plate) {
         if(plate.decreaseFood(appetite)){
-            //isHungry = false;
             satiety = true;
-            System.out.println(name + " поел из тарелки");
+            System.out.println(name + " ate from the plate!");
         }else{
-            /*
-            isHungry = true;
-            satiety =false;
-            */
-            System.out.println(name + " не хватает еды в тарелке");
+            System.out.println(name + " does not have enough food in the plate");
         };
     }
 
     public static void Satiety(Cat[] cats){
         for (int i = 0; i <cats.length ; i++) {
             if(cats[i].isSatiety()){
-                System.out.println(cats[i].getName() + " сыт");
+                System.out.println(cats[i].getName() + " ate");
             }else {
-                System.out.println(cats[i].getName() + " голоден");
+                System.out.println(cats[i].getName() + " hungry");
             }
         }
     }
